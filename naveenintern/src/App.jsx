9e93d84ae39285a3d1e6dd23b88f6f48474e68a2
Naveen1385 <vinayagamnaveenkumarsweet@>
2026-01-03 08:45:06 -0800

    Added components and page structure with Profile component

--- naveenintern/src/App.jsx
+++ naveenintern/src/App.jsx
@@ -1,10 +1,10 @@
 import Profile from "./Profile";
+
 function App() {
   return (
     <div>
-      <h1>Hello Naveen 🚀</h1>
-      <p>My React app is running successfully.</p>
-      <Profile name="Naveen" role="Frontend Intern" />
+      <h1>Main App</h1>
+      <Profile name="Naveen" role="Intern" />
     </div>
   );
 }
